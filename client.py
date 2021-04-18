566e8f6d74cf6eeba2df46f36cb01c5b6e3ccacc
Adam Ogorek <ogoreka@tcd.ie>
2021-04-18 14:55:01 +0100

    Added encrypting and decrypting files with AES, need to store the key, and parse the paths, currently assumes simpy filename instead of path

--- client.py
+++ client.py
@@ -1,4 +1,32 @@
 import pyrebase
+import os
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+
+
+def encrypt_file(filename):
+    file_in = open(filename,"rb")
+    data = file_in.read()
+    cipher = AES.new(key, AES.MODE_EAX)
+    encrypted_file, tag = cipher.encrypt_and_digest(data)
+
+    file_out = open("encrypted_" + filename, "wb")
+    [ file_out.write(x) for x in (cipher.nonce, tag, encrypted_file) ]
+    file_out.close()
+
+def decrypt_file(filename):
+    file_out = open(filename, "rb")
+    nonce, tag, encrypted_data = [ file_out.read(x) for x in (16, 16, -1) ]
+    file_out.close()
+    cipher = AES.new(key, AES.MODE_EAX, nonce)
+    decrypted_data = cipher.decrypt_and_verify(encrypted_data, tag)
+    os.remove(filename)
+
+    decrypted_file = open(filename, 'wb')
+    decrypted_file.write(decrypted_data)
+    decrypted_file.close()
+
 
 config = {
     "apiKey": "AIzaSyBtb-VLcnojAkq-0xZBGnCC0Dn08D4qq2w",
@@ -14,20 +42,24 @@ config = {
 firebase = pyrebase.initialize_app(config)
 
 cloud = firebase.storage()
+key = get_random_bytes(16)
+
+
 
 while(True):
     text = input("upload or download?\n")
     if(text == "upload"):
         path_on_cloud = input("What path to upload?\n")
-        print(path_on_cloud)
         path_on_device = input("Which file to upload?\n")
-        cloud.child("test2.txt").put(path_on_device)
+        encrypt_file(path_on_device)
+        cloud.child(path_on_cloud).put("encrypted_" + path_on_device)
+        os.remove("encrypted_" + path_on_device)
         print("done")
     elif(text == "download"):
         path_on_cloud = input("Which file to download?\n")
         path_on_device = input("Where to download?\n")
         cloud.child(path_on_cloud).download(path_on_device)
+        decrypt_file(path_on_device)
         print("done")
     else:
         print("Error")
-
